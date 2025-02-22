94c7bd98c5b59b3f0931525127e05b0280340586
nirge <nir@forsightrobotics.com>
2025-02-22 22:48:31 +0200

    *

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ async function run() {
             repo: _repo,
         });
 
-        core.info("~~~~~~~~~~~~~~~");
-        core.info(`Pull Requests for ${_repo}: `);
-        core.info(pullRequests);
+        github.info("~~~~~~~~~~~~~~~");
+        github.info(`Pull Requests for ${_repo}: `);
+        github.info(pullRequests);
 
         for (let el of pullRequests) {
             const pullRequestInfo = `${_repo} [${el.number}] ${el.title}`;
