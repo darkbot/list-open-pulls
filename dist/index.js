8e17b1ce76a8cda219d673cada1ca1f0220c8b2d
nirge <nir@forsightrobotics.com>
2025-02-22 23:04:12 +0200

    *

--- dist/index.js
+++ dist/index.js
@@ -9005,19 +9005,33 @@ const github = __nccwpck_require__(5438);
 async function run() {
     const myToken = core.getInput("token");
     const owner = core.getInput("owner");
-    const repo = core.getInput("repo");
+    const repos = core.getInput("repo").split(",");
 
-    const octokit = github.getOctokit(myToken)
+    const octokit = github.getOctokit(myToken);
 
-    const { data: pullRequests } = await octokit.request('GET /repos/{owner}/{repo}/pulls?state=open', {
-        owner,
-        repo,
-    });
-    let titles = []
-    for (let el of pullRequests) {
-        titles.push(el.title)
+    let _titles = [];
+    
+    for (let repo of repos) {
+        let _repo = repo.trim();
+        if (!_repo) {
+            continue;
+        }
+        if (_repo === "") {
+            continue;
+        }
+        const { data: pullRequests } = await octokit.request('GET /repos/{owner}/{_repo}/pulls?state=open', {
+            owner,
+            repo: _repo,
+        });
+
+        core.setOutput("dbg", `${pullRequests} ~ ${pullRequests.length}`);
+
+        for (let el of pullRequests) {
+            const pullRequestInfo = `${_repo} [${el.number}] ${el.title}`;
+            _titles.push(pullRequestInfo);
+        }
     }
-    core.setOutput("titles", titles);
+    core.setOutput("titles", _titles);
 }
 
 run();
