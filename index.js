0e226807e1be41e21517b05ac6be468b95b21356
nirge <nir@forsightrobotics.com>
2025-02-22 21:49:09 +0200

    test

--- index.js
+++ index.js
@@ -4,19 +4,27 @@ const github = require("@actions/github");
 async function run() {
     const myToken = core.getInput("token");
     const owner = core.getInput("owner");
-    const repo = core.getInput("repo");
+    const repos = JSON.parse(core.getInput("repos"));
 
-    const octokit = github.getOctokit(myToken)
+    const octokit = github.getOctokit(myToken);
 
-    const { data: pullRequests } = await octokit.request('GET /repos/{owner}/{repo}/pulls?state=open', {
-        owner,
-        repo,
-    });
-    let titles = []
-    for (let el of pullRequests) {
-        titles.push(el.title)
+    let _titles = [];
+    let _ids = [];
+    let _repo = [];
+    for (let repo of repos) {
+        const { data: pullRequests } = await octokit.request('GET /repos/{owner}/{repo}/pulls?state=open', {
+            owner,
+            repo,
+        });
+        for (let el of pullRequests) {
+            _ids.push(el.number);
+            _titles.push(el.title);
+            _repo.push(repo);
+        }
     }
-    core.setOutput("titles", titles);
+    core.setOutput("repo", _repo);
+    core.setOutput("ids", _ids);
+    core.setOutput("titles", _titles);
 }
 
 run();
